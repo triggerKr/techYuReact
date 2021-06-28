f09c28bc39535b7ba1f5ac55bb23d8108ff48663
kbntt <kbntt73@gmail.com>
2021-06-28 21:13:31 +0900

    2222

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React lee 1
+          Learn React lee 1 222
         </a>
       </header>
     </div>
