af729cb0b2222ee4f84b7f273bb3523dd26fa936
kbntt <kbntt73@gmail.com>
2021-06-28 21:06:27 +0900

    이응규

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React lee
         </a>
       </header>
     </div>
